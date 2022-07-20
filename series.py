9858c52c67d730155de85646a3cbe2b61e601d7e
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-19 17:34:48 -0700

    completed sum_series function

--- series.py
+++ series.py
@@ -17,4 +17,9 @@ def lucas(n):
 
 
 def sum_series(n, first=0, second=1):
-    return 1
+    if n == 0:
+        return first
+    elif n == 1:
+        return second
+    else:
+        return sum_series(n - 1, first, second) + sum_series(n - 2, first, second)
