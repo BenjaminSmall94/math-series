9a8dcbde108d33cf35177f9afd1a6162f962a406
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-19 17:45:07 -0700

    added docstrings to series.py

--- series.py
+++ series.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
+    """Delivers the nth element from a fibonacci sequence
+
+    :param n: (int) defines the nth element to be requrests from the fibonacci sequence
+
+    :return: (int) the value of the nth element of a fibonacci sequence
+    """
     if n == 0:
         return 0
     elif n == 1:
@@ -8,6 +14,12 @@ def fibonacci(n):
 
 
 def lucas(n):
+    """Delivers the nth element from a lucas sequence
+
+    :param n: (int) defines the nth element to be requrests from the lucase sequence
+
+    :return: (int) the value of the nth element of a lucas sequence
+    """
     if n == 0:
         return 2
     elif n == 1:
@@ -17,6 +29,14 @@ def lucas(n):
 
 
 def sum_series(n, first=0, second=1):
+    """Delivers the nth element from a custom mathematical sequence
+
+    :param n: (int) defines the nth element to be requrests from the custom mathematical sequence
+    :param first: (int) defines the value of the first element of a custom mathematical sequence
+    :param second: (int) defines the value of the second element of a custom mathematical sequence
+
+    :return: (int) the value of the nth element of a custom mathematical sequence
+    """
     if n == 0:
         return first
     elif n == 1:
