972a5677e504f0ae146f61f717419a74f0fb9682
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-19 17:27:35 -0700

    completed fibonacci function

--- series.py
+++ series.py
@@ -1,5 +1,10 @@
 def fibonacci(n):
-    return 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
